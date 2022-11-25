cd880342fb2c058f1093cf748dd415f2b91ea962
Desmondmies <desmondmies@gmail.com>
2022-11-25 12:19:03 +0100

    init commit

---
+++ src/main/java/univtln/fr/i111/jlaffaill662/App.java
@@ -0,0 +1,13 @@
+package univtln.fr.i111.jlaffaill662;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
